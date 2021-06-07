cdd4cfe3926db1f863ff79c00f86ba3703194c66
Konrad Ptak <xptak.konrad@gmail.com>
2021-06-07 20:31:41 +0200

    Dodanie listy użytowników

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<table class="table table-borderless">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Imię</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+      @foreach($users as $user)
+    <tr>
+      <th scope="row">{{$user -> id}}</th>
+      <td>{{$user -> name}}</td>
+      <td>{{$user -> email}}</td>     
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+</div>
+@endsection
