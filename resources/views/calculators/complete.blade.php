265c48e4f1ab7a00e54f5961d263cd16d36009a8
Konrad Ptak <xptak.konrad@gmail.com>
2022-02-02 16:42:56 +0100

    Dalsze poprawki zamówien

--- resources/views/calculators/complete.blade.php
+++ resources/views/calculators/complete.blade.php
@@ -9,10 +9,13 @@
 
 
                     <div class="card-header">{{ __('Kalkulacja') }}</div>
-                    <div class="card-body">
-                        {{var_dump($array['kalkulacja'])}}
-                        <br>
-                        {{var_dump($_POST)}}
+                    <div class="card-body mx-auto">
+                            <h1>Dziękujemy za złożenie zamówienia.</h1><br>
+                            <div class="row justify-content-center">
+                                <a class="float-lg-none" href="{{ route('orders.index') }}">
+                                    <button type="button" class="btn btn-primary btn-lg">Moje zamówienia</button>
+                                </a>
+                            </div>
                     </div>
                 </div>
             </div>
